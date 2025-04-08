f3b07fce4634070d8f1cd289504b04bc5a3542cb
Joe Invisible <invisikillerjoe@gmail.com>
2025-04-09 00:57:36 +0800

    typo

--- backend/src/services/EventService.ts
+++ backend/src/services/EventService.ts
@@ -6,57 +6,41 @@ import { LatLng } from "@googlemaps/google-maps-services-js";
 import { Establishment } from "../../../shared/Location";
 
 export class EventService {
-  /**
-   * Completes and posts the event information as retrieved from the client-end form.
-   * @param eventFormData client-end form, where `id` and `meetupLocations` are not yet
-   * populated
-   * @returns the assigned ID of the newly created event.
-   */
-  static async createEvent(eventFormData: Partial<Event>): Promise<string> {
-    // Generate a unique ID for the new event
-    const eventId = Math.random().toString(36).substr(2, 6);
+	/**
+	 * Completes and posts the event information as retrieved from the client-end form.
+	 * @param eventFormData client-end form, where `id` and `meetupLocations` are not yet
+	 * populated
+	 * @returns the assigned ID of the newly created event.
+	 */
+	static async createEvent(eventFormData: Partial<Event>): Promise<string> {
+		// Generate a unique ID for the new event
+		const eventId = Math.random().toString(36).substr(2, 6);
 
-    const newEvent: Event = {
-      id: eventId,
-      name: eventFormData.name,
-      dateRange: eventFormData.dateRange,
-      timeRange: eventFormData.timeRange,
-      area: eventFormData.area,
-      responseDeadline: eventFormData.responseDeadline,
-      meetupLocations: await this.initMeetupLocations(eventFormData),
-      participants: [],
-      notified: false,
-      createdBy: eventFormData.createdBy || null,
-      isPublic:
-        eventFormData.isPublic !== undefined ? eventFormData.isPublic : true,
-    };
+		const newEvent: Event = {
+			id: eventId,
+			name: eventFormData.name,
+			dateRange: eventFormData.dateRange,
+			timeRange: eventFormData.timeRange,
+			area: eventFormData.area,
+			responseDeadline: eventFormData.responseDeadline,
+			meetupLocations: await this.initMeetupLocations(eventFormData),
+			participants: [],
+			notified: false,
+			createdBy: eventFormData.createdBy || null,
+			isPublic: eventFormData.isPublic !== undefined ? eventFormData.isPublic : true,
+		};
 
-    console.log(newEvent.meetupLocations);
+		console.log(newEvent.meetupLocations);
 
-    return await EventRepository.createEvent(eventId, newEvent);
-  }
+		return await EventRepository.createEvent(eventId, newEvent);
+	}
 
-<<<<<<< HEAD
 	private static async initMeetupLocations(eventFormData: Partial<Event>): Promise<Establishment[]> {
 		const latlng = { lat: eventFormData.area.lat, lng: eventFormData.area.lng } as LatLng;
 		return (await searchPlaces(latlng, 500, "cafe", "bus_station")).slice(0, 5).sort((e1, e2) => e1.distance - e2.distance);
 	}
-=======
-  private static async initMeetupLocations(
-    eventFormData: Partial<Event>
-  ): Promise<Establishment[]> {
-    const latlng = {
-      lat: eventFormData.area.lat,
-      lng: eventFormData.area.lng,
-    } as LatLng;
-    return (await searchPlaces(latlng, 500, "cafe", "bus_station")).slice(0, 5);
-  }
->>>>>>> 10af740b49b4156d13a45490fb80ded5bfa0b991
 
-  static async getExpiredEvent(): Promise<Event[]> {
-    return await EventRepository.getEvents(
-      where("responseDeadline", "<=", new Date().toISOString()),
-      where("notified", "==", false)
-    );
-  }
+	static async getExpiredEvent(): Promise<Event[]> {
+		return await EventRepository.getEvents(where("responseDeadline", "<=", new Date().toISOString()), where("notified", "==", false));
+	}
 }
