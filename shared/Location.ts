50b187a2e2866cd7f8bd9a53da2244400a8e3694
bliblublob <huangyuxuan121122@gmail.com>
2025-04-10 17:16:41 +0800

    feat: simple ai feature

--- shared/Location.ts
+++ shared/Location.ts
@@ -1,16 +1,16 @@
 export interface Location {
-	name: string;
-	address?: string;
-	lat: number;
-	lng: number;
+  name: string;
+  address?: string;
+  lat: number;
+  lng: number;
 }
 
 export interface Establishment {
-	id?: string;
-	location: Location;
-	distance: number;
-	rating: number;
-	category: string[];
-	votedBy: Array<string>;
-	link: string;
+  id?: string;
+  location: Location;
+  distance: number;
+  rating: number;
+  category: string[];
+  votedBy: Array<string>;
+  link: string;
 }
